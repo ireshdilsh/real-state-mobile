6c1f9b02888fe4a61823ab40d7548d29b1e2943d
ireshdilsh <ireshdilshan46@gmail.com>
2025-10-06 16:55:46 +0530

    chore: update dependencies to latest versions for expo, react, and react-native

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>This App Create My Second year third semester project for</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text:{
+    textAlign:'center',
+    color:'red',
+    width:'50%'
+  }
 });
